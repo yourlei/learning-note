b4cbdaba62d26e7ca020ab7cf9865ee6ee6a34ba
yourlei <yourlin127@gmail.com>
2016-07-05 23:57:09 +0800

    chapter06 ui-router and shell

---
+++ angularJS/chapter06/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('ui-router', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/state1');
+	$stateProvider.state('state1', {
+		url: '/state1', 
+		templateUrl: 'include/page1.html'
+	})
+	.state('state1.list', {
+		url: '/list',
+		templateUrl: 'include/list.html',
+		controller: function($scope) {
+			$scope.items = ['white', 'black', 'gray'];
+		}
+	})
+	.state('state2', {
+		url: '/state2', 
+		templateUrl: 'include/page2.html'
+		// template: '<h1>hello angular-ui-router</h1>'
+		// controller: 'state2'
+	});
+});
